c48b5ee5ec8e8a4294b1ddbdcd3483254ae0e85f
Lihcruz <livia.dugonski@escola.pr.gov.br>
2025-06-29 23:18:52 +0000

    sexto commit

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
-botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-  const texto = document.querySelector("#entrada-de-texto").value;
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavrasChave = processaTexto(texto);
 
-  const campoResultado = document.querySelector("#resultado-palavrachave");
-
-  const palavrasChave = processaTexto(texto);
-
-  campoResultado.textContent = palavrasChave.join(", ");
+    campoResultado.textContent = palavrasChave.join(", ");
 }
 
 function processaTexto(texto) {
-  let palavras = texto.split(/\P{L}+/u);
-  const frequencias = contaFrequencias(palavras);
-  let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    let palavras = texto.split(/\P{L}+/u);
 
-   function ordenaPalavra(p1, p2) {
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
+    }
+
+    palavras = tiraPalavrasRuins(palavras);
+
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+    function ordenaPalavra(p1, p2) {
         return frequencias[p2] - frequencias[p1];
     }
 
@@ -36,4 +41,15 @@ function contaFrequencias(palavras) {
         }
     }
     return frequencias;
+}
+
+function tiraPalavrasRuins(palavras) {
+    const PALAVRAS_RUINS = new Set(["para", "uma", "nós"]);
+    const palavrasBoas = [];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
 }
